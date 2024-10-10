79d583dc00a204f3341c64fc40a1f46241d64683
Malkevich Liliya E <MalkevichLE@bsu.by>
2024-10-10 16:43:48 +0300

    added contacts

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,26 @@
 const swiper = new Swiper(".swiper", {
   // Optional parameters
   loop: true,
-  slidesPerView: 4,
-  spaceBetween: 30,
+  slidesPerView: 1,
+  spaceBetween: 10,
 
   // Navigation arrows
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    720: {
+      slidesPerView: 2,
+      spaceBetween: 15,
+    },
+    1110: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    1430: {
+      slidesPerView: 4,
+      spaceBetween: 30,
+    },
   },
 });
